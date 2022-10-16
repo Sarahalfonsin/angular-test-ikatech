af8a26b693dc5c7fe3b4442ea8019b62f1f67c39
sarahalfonsin <sarahalfonsin@hotmail.com>
2022-10-16 20:10:52 -0300

    guardo carrousel por libreria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MatSliderModule } from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,29 +10,12 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductoPageComponent } from './pages/producto-page/producto-page.component';
 
 import { FormsModule } from '@angular/forms';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatCardModule } from '@angular/material/card';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FooterComponent } from './components/footer/footer.component';
 import { FooterModule } from './components/footer/footer.module';
-
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,25 +33,8 @@ import { FooterModule } from './components/footer/footer.module';
     FooterModule,
     BrowserAnimationsModule,
     MatButtonToggleModule,
-    MatSliderModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatListModule,
     MatIconModule,
-    MatButtonModule,
-    MatExpansionModule,
-    MatFormFieldModule,
-    MatTableModule,
-    MatDialogModule,
-    MatSnackBarModule,
-    MatInputModule,
-    MatProgressSpinnerModule,
-    MatCardModule,
-    MatGridListModule,
-    MatChipsModule,
-    MatBadgeModule,
-    MatMenuModule,
-    MatStepperModule,
+    NgxGalleryModule
   ],
 
   providers: [],
--- src/app/pages/producto-page/producto-page.component.ts
+++ src/app/pages/producto-page/producto-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  NgxGalleryAnimation,
+  NgxGalleryImage,
+  NgxGalleryOptions,
+} from '@kolkov/ngx-gallery';
 import { IZapato, zapatos } from 'src/app/models/IZapato.model';
 import { buyBagService } from 'src/app/services/buyBag/buyBag.service';
 
@@ -15,16 +20,63 @@ export class ProductoPageComponent implements OnInit {
   productoR: IZapato[];
   productReco: IZapato[];
   itembuy: number = 0;
- 
+
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[] = [];
+
   constructor(private buyBag: buyBagService) {}
   ngOnInit(): void {
+    this.galleryOptions = [
+      {
+        width: '600px',
+        height: 'auto',
+        thumbnailsColumns: 6,
+        imageAnimation: NgxGalleryAnimation.Slide,
+      },
+      // max-width 800
+      {
+        breakpoint: 800,
+        width: '100%',
+        height: '600px',
+        imagePercent: 80,
+        thumbnailsPercent: 20,
+        thumbnailsMargin: 20,
+        thumbnailMargin: 20,
+      },
+      // max-width 400
+      {
+        breakpoint: 400,
+        preview: false,
+      },
+    ];
+
     this.productoR = this.producto.filter(
       (producto) => producto.id > 5 && producto.id < 12
     );
-    console.log(this.productoR);
     this.productReco = this.producto.filter(
       (producto) => producto.id > 5 && producto.id < 10
     );
+    this.initialize();
+  }
+  initialize() {
+    this.imagesChargue();
+  }
+  imagesChargue() {
+    let test;
+    let array: [{}] = [{}];
+    test = this.productoR?.forEach((e, i) => {
+      let min = {
+        small: '../../../assets/img/' + e.foto,
+        medium: '../../../assets/img/' + e.foto,
+        big: '../../../assets/img/' + e.foto,
+      };
+      array.push(min);
+    });
+
+    array.map((e) => {
+      this.galleryImages.push(e);
+    });
+    this.galleryImages.splice(0, 1);
   }
   addShopping() {
     this.itembuy += 1;
